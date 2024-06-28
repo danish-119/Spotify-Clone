fd2c5cd01944954449286dec90bea3574aa16124
Muhammad Danish <mdanishateeq@gmail.com>
2024-06-29 01:38:50 +0500

    _

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 console.log("Welcome to Spotify Clone!");
+
 let acitveSong = new Audio();
+let songs = [];
+
 // ---------------------------------------------------------------------------------
 async function getSongs() {
-  let a = await fetch("http://127.0.0.1:3000/Inventory/Songs/");
+  let a = await fetch("http://127.0.0.1:5500/Inventory/Songs/");
   let response = await a.text();
-  // console.log(response);
 
   let div = document.createElement("div");
   div.innerHTML = response;
   let as = div.getElementsByTagName("a");
-  // console.log(as);
 
   let songs = [];
   for (let i = 0; i < as.length; i++) {
@@ -19,7 +20,6 @@ async function getSongs() {
       );
     }
   }
-  // console.log(songs);
 
   return songs;
 }
@@ -27,13 +27,18 @@ async function getSongs() {
 //--------------------------------------------------------------------------------------
 function playMusic(track) {
   acitveSong.src = "/Inventory/Songs/" + track + ".mp3";
+  document.querySelectorAll(".play").forEach((element) => {
+    element.src = "Inventory/Icons/play.svg";
+  });
+  document.querySelector(".play-pause").src = "Inventory/Icons/pause.svg";
+  document.querySelector(".song-name").innerHTML =
+    track + " - Sidhu Moose Wala";
   acitveSong.play();
 }
 
 // -------------------------------------------------------------------------------------
 async function main() {
-  let songs = await getSongs();
-  // console.log(songs);
+  songs = await getSongs();
 
   let element = document.querySelector(".songs-list");
   for (const song of songs) {
@@ -50,11 +55,36 @@ async function main() {
                 </li>`;
   }
 
+  // ------------
   Array.from(document.querySelectorAll(".song")).forEach((e) => {
     e.querySelector(".play").addEventListener("click", () => {
       console.log(e.querySelector(".song-title").innerHTML);
       playMusic(e.querySelector(".song-title").innerHTML);
+      e.querySelector(".play").src = "Inventory/Icons/pause.svg";
     });
+  });
+
+  //---------
+  document.querySelector(".play-pause").addEventListener("click", () => {
+    console.log("play-pause clicked");
+    if (acitveSong.paused) {
+      acitveSong.play();
+      document.querySelector(".play-pause").src = "Inventory/Icons/pause.svg";
+    } else {
+      acitveSong.pause();
+      document.querySelector(".play-pause").src = "Inventory/Icons/play.svg";
+    }
+  });
+
+  //------------
+  document.querySelector(".volume-on").addEventListener("click", () => {
+    if (acitveSong.volume != 0) {
+      acitveSong.volume = 0;
+      document.querySelector(".volume-on").src = "Inventory/Icons/mute.svg";
+    } else {
+      acitveSong.volume = 1;
+      document.querySelector(".volume-on").src = "Inventory/Icons/volume.svg";
+    }
   });
 }
 
