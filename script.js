303a2b4d23dc502578ca7d8055afc5eba1c0a4a1
Muhammad Danish <mdanishateeq@gmail.com>
2024-06-28 09:31:47 +0500

    _

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Welcome to Spotify Clone!");
 
+// ---------------------------------------------------------------------------------
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:3000/Inventory/Songs/");
   let response = await a.text();
@@ -13,12 +14,40 @@ async function getSongs() {
   let songs = [];
   for (let i = 0; i < as.length; i++) {
     if (as[i].href.endsWith(".mp3")) {
-      songs.push(as[i].href);
+      songs.push(
+        as[i].href.split("Songs/")[1].replaceAll("%20", " ").split(".mp3")[0]
+      );
     }
   }
-  console.log(songs);
+  // console.log(songs);
 
   return songs;
 }
 
-getSongs();
+// const play = function playSong(){
+//   let song = document.querySelector(".song").innerHTML;
+//   console.log(song);
+// }
+
+// -------------------------------------------------------------------------------------
+async function main() {
+  let songs = await getSongs();
+  console.log(songs);
+
+  let element = document.querySelector(".songs-list");
+  for (const song of songs) {
+    element.innerHTML = element.innerHTML + `           <li class="song">
+                    <img src="Inventory/Icons/music.svg" alt="">
+                    <span class="song-info">
+                        <div class="song-title">${song}</div>
+                        <span>Sidhu Moose Wala</span>
+                    </span>
+                    <span class="play-now">Play Now</span>
+                    <img class="play" src="Inventory/Icons/play.svg" alt="">
+                </li>`;
+  }
+  // var audio = new Audio(songs[2]);
+  // audio.play();
+}
+
+main();
